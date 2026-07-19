7cc0ee1eb5e31b93d295d1fa0c06f05456c4e76e
elfidiana240180097-prog <elfidiana240180097@mhs.unimal.ac.id>
2026-07-19 16:48:23 +0700

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,23 +4,21 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
-
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-            Paginator::useBootstrapFive();
+        Paginator::useBootstrapFive();
+
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
