4db462c3d9697f9a314f4fdda7725efaac22a988
elfidiana240180097-prog <elfidiana240180097@mhs.unimal.ac.id>
2026-07-19 18:06:20 +0700

    Fix article country relationship

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Article extends Model
 {
@@ -22,4 +23,9 @@ class Article extends Model
         'published_at'
 
     ];
+
+    public function country(): BelongsTo
+    {
+        return $this->belongsTo(Country::class);
+    }
 }
